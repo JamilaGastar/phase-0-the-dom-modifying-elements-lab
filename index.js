69ff6fc7a3f39e54dadab877d6f06f9d7b34c86a
jamilagastar <jamilagastar@gmail.com>
2022-05-01 14:32:50 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.className = "victory";
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Jamila is the champion";
